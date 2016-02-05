eeabb12d8f10bd5d7cfbcd1b730bf7a6a91ea234
Julien Sergent <julien.sergent@prep-app.com>
2016-02-05 16:06:13 +0100

    Autofix main-container min-width in js.

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -1,5 +1,10 @@
 ( function( globa, $, undef ) {
     $( document ).ready( function() {
+        var $badgesContainer = $( '.badges-container' ).first();
+        $( '#main-container' ).css({
+            "min-width": $badgesContainer.find( '.bagde-container' ).width() * $badgesContainer.children().length
+        })
+
         $( '.scaled' ).css({
             transform: 'scale(1)',
             opacity: '1'
