6aaedd09197dedaaf9289b304ff6b9bbacc6744e
Julien Sergent <julien.sergent@prep-app.com>
2016-02-05 14:30:35 +0100

    Scale effect.

---
+++ resources/dist/app.min.js
@@ -0,0 +1,8 @@
+( function( globa, $, undef ) {
+    $( document ).ready( function() {
+        $( '.scaled' ).css({
+            transform: 'scale(1)',
+            opacity: '1'
+        });
+    });
+})( this, jQuery );
